d549ed332d2356ffd589619c5255408b4dd5895b
Mallory Howard <mallory1313@msn.com>
2024-03-22 20:02:39 -0600

    added instructions to readme. Created fields/columns for various model files. Added sequelize connection to server.js file.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # E-commerce Back End
 
+## Installation for developers
+Install Node.js, if needed.     
+
+Create a .gitignore file and include "node_modules", ".env", ".vscode", "package-lock.json", and ".DS_Store" in it, so that these files aren't tracked or uploaded to GitHub. Be sure to create your .gitignore file before installing any npm dependencies.     
+
+Make sure that your repo includes a package.json with the required dependencies. You can create one by running "npm init" when you first set up the project, before installing any dependencies.     
+
+This application requires the Sequelize package. To install Sequelize, navigate to your terminal and enter "npm install sequelize".  
+
+This application requires the MySQL2 package. To install MySQL2, navigate to your terminal and enter "npm install --save mysql2".
+
+This application requires the dotenv package. To install dotenv, navigate to your terminal and enter "npm i dotenv".
+
+
 ## Credits
 Started Code Sourced From: https://github.com/coding-boot-camp/fantastic-umbrella
--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,46 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        is: {
+          args: /^\d+(\.\d+)?$/, // Regex pattern for decimal numbers -- maybe use isnumeric instead?
+          msg: "Price must be a valid decimal number." // Custom error message
+        }
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
